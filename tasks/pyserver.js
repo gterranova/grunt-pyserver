712ca1ee0c9678cc2e3e6cfec8d3de7f78e23a4f
gterranova <gianpaoloterranova@gmail.com>
2013-10-22 19:31:06 +0200

    Initial commit

---
+++ tasks/pyserver.js
@@ -0,0 +1,35 @@
+/*
+ * grunt-pyserver
+ * https://github.com/gterranova/grunt-pyserver
+ *
+ * Copyright (c) 2013 Gianpaolo Terranova
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    var spawn = require('child_process').spawn;
+    var pyserver = require('node-pyserver-bin');
+    var async = require('async');
+    
+    var start_server = function(port, root, done)  {
+        grunt.log.writeln('Starting server...');
+        var pyserverArgs = [pyserver.path, port, root];
+        grunt.verbose.writeflags(pyserverArgs, 'Options');
+
+        async.forever(function(){
+            spawn('python.exe', pyserverArgs, {stdio: 'inherit'});
+        }, function(){});
+    };
+  
+    grunt.registerTask('pyserver', 'Start pyserver', function () {            
+        var options = this.options({
+            port: 8000,
+            documentRoot: ''
+        });
+                
+        start_server(options.port, options.documentRoot);
+    });
+};
